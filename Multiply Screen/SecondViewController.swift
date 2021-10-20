26158f0841722fe402e61b71492cbccdc16067aa
Onuralp <xonuraralpx@gmail.com>
2021-10-20 15:10:20 +0300

    Multiply screen

---
+++ Multiply Screen/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  Multiply Screen
+//
+//  Created by Ceren Çapar on 16.09.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var labelName: UILabel!
+    var myName = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        labelName.text = myName
+
+    }
+    
+
+ 
+
+}
--- Multiply Screen/ViewController.swift
+++ Multiply Screen/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textFieldName: UITextField!
+    var userName = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       
+        
     }
-
+    @IBAction func buttonNext(_ sender: Any) {
+        userName = "Name: \(textFieldName.text!)"
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC"{
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.myName = userName
+        }
+    }
 
 }
 
